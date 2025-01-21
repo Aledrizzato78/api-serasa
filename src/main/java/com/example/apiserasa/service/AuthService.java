3588389c8ed10ad061acdf7e2478eec7016b2880
Aledrizzato78 <aledrizzato@gmail.com>
2025-01-21 10:28:32 -0300

    alteração 21.01

--- src/main/java/com/example/apiserasa/service/AuthService.java
+++ src/main/java/com/example/apiserasa/service/AuthService.java
@@ -2,19 +2,25 @@ package com.example.apiserasa.service;
 
 import com.example.apiserasa.util.JwtUtil;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 @Service
 public class AuthService {
 
     private final JwtUtil jwtUtil;
 
+    @Value("${spring.security.user.name}")
+    private String defaultUsername;
+
+    @Value("${spring.security.user.password}")
+    private String defaultPassword;
+
     public AuthService(JwtUtil jwtUtil) {
         this.jwtUtil = jwtUtil;
     }
 
     public String autenticarUsuario(String username, String password) {
-        // Aqui você deve verificar usuário/senha no banco (mockado no exemplo)
-        if ("admin".equals(username) && "password".equals(password)) {
+        if (defaultUsername.equals(username) && defaultPassword.equals(password)) {
             return jwtUtil.generateToken(username);
         }
         throw new RuntimeException("Usuário ou senha inválidos!");
